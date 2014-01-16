87cb3d72a50e7d8f123838ff4a49524e8e2f82d7
Jonathan van Wunnik <jonathan@artlantis.nl>
2014-01-16 09:52:04 +0100

    Naming conventions.

--- site/templates/default.php
+++ site/templates/default.php
@@ -2,16 +2,24 @@
 
 	<?php snippet('banner'); ?>
 
+	<div class="CoverImage FluidEmbed FluidEmbed--3by2 FluidEmbed--compact16by9 FluidEmbed--medium2by1 FluidEmbed--large3by1"
+		style="background-image: url('<?php echo $pages->find('home')->images()->filterBy('title', '*=', 'cover-')->shuffle()->first()->url(); ?>')">
+	</div>
+
+	<!-- <div class="FluidEmbed FluidEmbed--3by2 FluidEmbed--compact16by9 FluidEmbed--medium2by1 FluidEmbed--large3by1">
+		<img src="<?php echo $pages->find('home')->images()->filterBy('title', '*=', 'cover-')->shuffle()->first()->url(); ?>" class="FluidEmbed-item">
+	</div> -->
+
 	<div class="u-container">
 
-		<article role="main" class="Copy">
+		<div role="main" class="Copy">
 
 			<h1><?php echo smartypants(widont($page->title())); ?></h1>
 
 			<?php echo kirbytext($page->intro()); ?>
 			<?php echo kirbytext($page->text()); ?>
 
-		</article><!--/main/Copy-->
+		</div><!--/main/Copy-->
 
 		<?php snippet('share_page'); ?>
 
