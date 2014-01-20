cee366c35af013a20985b58689a4a0aade38c350
Jonathan van Wunnik <jonathan@artlantis.nl>
2014-01-20 09:02:36 +0100

    Fix issue with scripts grunt task.

--- grunt/tasks/develop.js
+++ grunt/tasks/develop.js
@@ -1,17 +1,11 @@
 module.exports = function(grunt) {
 
 	grunt.registerTask('develop', [], function () {
-		grunt.loadNpmTasks('grunt-contrib-sass');
-		grunt.loadNpmTasks('grunt-autoprefixer');
-		grunt.loadNpmTasks('grunt-contrib-concat');
-		grunt.loadNpmTasks('grunt-contrib-jshint');
 		grunt.loadNpmTasks('grunt-contrib-watch');
 		grunt.task.run(
-			'sass:debug',
-			'autoprefixer',
-			'concat:forhint',
-			'concat:dist',
-			'jshint:dist',
+			'sass-concat',
+			'scripts-concat',
+			'scripts-hint',
 			'watch'
 		);
 	});
