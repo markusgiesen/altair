6a2377cf571698284fd40d824d87a1a405904352
Jonathan van Wunnik <jonathan@artlantis.nl>
2014-01-27 16:28:49 +0100

    Update pagination snippet logic.

--- site/snippets/pagination.php
+++ site/snippets/pagination.php
@@ -14,40 +14,48 @@
 <div role="pagination" class="Pagination">
 
 	<p class="Pagination-count">
-		<strong><?php echo $pagination->countItems(); ?></strong> results / showing <strong><?php echo $pagination->numStart(); ?></strong> &ndash; <strong><?php echo $pagination->numEnd(); ?></strong>
+		<strong><?php echo $pagination->countItems(); ?></strong> results / showing <strong><?php echo $pagination->numStart(); ?></strong> &ndash; <strong><?php echo ($pagination->countItems() < $pagination->numEnd()) ? $pagination->countItems() : $pagination->numEnd(); ?></strong>
 	</p>
 
-	<?php if($pagination->countItems() > $pagination->limit ): ?>
+	<?php if($pagination->countItems() > $pagination->limit): ?>
 	<nav>
-		<?php if(!$pagination->isFirstPage()): ?>
-			<a class="Pagination-first" href="<?php echo $pagination->firstPageURL() ?>">&larr;&larr; First</a>
-		<?php else: ?>
-			<span class="Pagination-first">&larr;&larr; First</span>
-		<?php endif ?>
+		<?php if(isset($range) && $pagination->countPages() > $range): ?>
+			<?php if(!$pagination->isFirstPage()): ?>
+				<a href="<?php echo (str::substr($pagination->firstPageURL(),0,-7)); ?>" class="Pagination-first">&larr;&larr; First</a>
+			<?php else: ?>
+				<span class="Pagination-first">&larr;&larr; First</span>
+			<?php endif; ?>
+		<?php endif; ?>
 
 		<?php if($pagination->hasPrevPage()): ?>
-			<a href="<?php echo $pagination->prevPageURL(); ?>" class="Pagination-prev">&larr; Previous</a>
+			<?php if($pagination->page() == 2): ?>
+				<a href="<?php echo (str::substr($pagination->firstPageURL(),0,-7)); ?>" class="Pagination-prev">&larr; Previous</a>
+			<?php else: ?>
+				<a href="<?php echo $pagination->prevPageURL(); ?>" class="Pagination-prev">&larr; Previous</a>
+			<?php endif; ?>
 		<?php else: ?>
 			<span class="Pagination-prev">&larr; Previous</span>
 		<?php endif; ?>
 
 		<?php if(isset($range) && $pagination->countPages() > 1): ?>
-			<?php foreach($pagination->range($range) as $range): ?>
-				<a href="<?php echo $pagination->pageURL($range) ?>" class="Pagination-range<?php if($pagination->page() == $range) echo ' is-active' ?>"><?php echo $range ?></a></li>
+			<?php foreach($pagination->range($range) as $range_num): ?>
+				<a href="<?php echo $pagination->pageURL($range_num) ?>" class="Pagination-range<?php if($pagination->page() == $range_num) echo ' is-active' ?>"><?php echo $range_num ?></a></li>
 			<?php endforeach ?>
 		<?php endif; ?>
 
 		<?php if($pagination->hasNextPage()): ?>
-			<a href="<?php $pagination->nextPageURL(); ?>" class="Pagination-next">Next &rarr;</a>
+			<a href="<?php echo $pagination->nextPageURL(); ?>" class="Pagination-next">Next &rarr;</a>
 		<?php else: ?>
 			<span class="Pagination-next">Next &rarr;</span>
 		<?php endif; ?>
 
-		<?php if(!$pagination->isLastPage()): ?>
-			<a class="Pagination-last" href="<?php echo $pagination->lastPageURL() ?>">Last &rarr;&rarr;</a>
-		<?php else: ?>
-			<span class="Pagination-last">Last</span>
-		<?php endif ?>
+		<?php if(isset($range) && $pagination->countPages() > $range): ?>
+			<?php if(!$pagination->isLastPage()): ?>
+				<a href="<?php echo $pagination->lastPageURL() ?>" class="Pagination-last">Last &rarr;&rarr;</a>
+			<?php else: ?>
+				<span class="Pagination-last">Last &rarr;&rarr;</span>
+			<?php endif; ?>
+		<?php endif; ?>
 	</nav>
 	<?php endif; ?>
 
