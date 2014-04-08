8710d50957e2c0ac0751bac955459e34536b89e4
Jonathan van Wunnik <jonathan@artlantis.nl>
2014-04-08 08:56:06 +0200

    Update CC license link to version 4.0.

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -22,7 +22,7 @@ endif;
 	<div role="contentinfo" class="ContentInfo">
 		<footer>
 			<?php echo kirbytext($site->copyright()); ?>
-			<p><small>All contents licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/" title="Creative Commons Attribution-Non-Commercial-No-Derivs 3.0 Unported">CC BY-NC-ND license</a>.</small></p>
+			<p><small>All contents licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/" title="Creative Commons Attribution-Non-Commercial-No-Derivs 4.0 International">CC BY-NC-ND license</a>.</small></p>
 			<p>mobile</p>
 		</footer>
 	</div>
