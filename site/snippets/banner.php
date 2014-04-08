898e584d63f99c5a2d4cd1499c250260278bab76
Jonathan van Wunnik <jonathan@artlantis.nl>
2014-04-08 09:45:05 +0200

    Update banner (HTML5 outline).

--- site/snippets/banner.php
+++ site/snippets/banner.php
@@ -5,8 +5,9 @@
 ////////////////////////////////////////////////////////// ?>
 
 <header role="banner" class="Banner">
-	<?php if($page->isHomePage()): echo '<h1'; else: echo '<a href="' . $site->url() . '" title="Return to the homepage" rel="home"'; endif; echo ' class="Masthead">'?>
+	<?php if($page->isHomePage()): echo '<span'; else: echo '<a href="' . $site->url() . '" title="Return to the homepage" rel="home"'; endif; echo ' class="Masthead">'?>
 		<img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Altair"/>
-	<?php if($page->isHomePage()): echo $site->title() . '</h1>'; else: echo '</a>'; endif; ?>
+		<?php // echo $site->title(); ?>
+	<?php if($page->isHomePage()): echo '</span>'; else: echo '</a>'; endif; ?>
 	<a href="#NavMain" class="NavMainToggle NavMainToggle--open js-navMainShow">Jump to navigation</a>
 </header>
